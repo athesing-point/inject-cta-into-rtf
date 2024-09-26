d9a6fe5c0261afb0f11a6a34ad4cdd070590db19
Austin Thesing <athesing@point.com>
2024-09-26 11:23:10 -0600

    Updated console logs

--- element-inject.js
+++ element-inject.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   // Access the rich text field by its class
   var rtf = document.querySelector(".blog-rich-text");
   if (!rtf) {
-    console.error("Failed to find the rich text field with class '.blog-rich-text'");
+    console.error("Failed to find RTF element.");
     return;
   }
 
   // Find the element by its ID
   var myElement = document.getElementById("injected-cta");
   if (!myElement) {
-    console.error("Failed to find the element with ID 'injected-cta'");
+    console.error("Failed to find CTA element.");
     return;
   }
 
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var blogCategoryElement = document.querySelector(".blog-category-text");
   if (blogCategoryElement && blogCategoryElement.textContent.toLowerCase().includes("homeowner stories")) {
     myElement.style.display = "none";
-    console.log("CTA hidden due to 'homeowner stories' category");
+    console.log("CTA insertion disabled due to category.");
     return;
   }
 
@@ -82,8 +82,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Insert the wrapped CTA at the determined position
     children[insertIndex].insertAdjacentElement("beforebegin", ctaWrapper);
 
-    console.log("Inserted 'injected-cta' between H2 elements with content or at 33% mark.");
+    // console.log("Inserted 'injected-cta' between H2 elements with content or at 33% mark.");
   } else {
-    console.error("Either the rich text field or the element to be injected is missing.");
+    console.error("CTA or RTF not found check mappings.");
   }
 });
